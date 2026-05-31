697cf0ddd90df5edce8261368a3ee63f52336eb8
Valdan Conceição França <valdandaconceicao@gmail.com>
2026-05-31 15:12:46 -0300

    Corrige bug de crash ao minimizar ou maximizar a janela no Windows

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,36 +11,9 @@ public partial class App : Application
     {
         var window = new Window(new AppShell())
         {
-            Title = "Cadastro de Eventos - Dev Valdan Conceição França",
-            MinimumWidth = 400,
-            MinimumHeight = 600
+            Title = "Cadastro de Eventos - Dev Valdan Conceição França"
         };
 
-        window.Destroying += OnWindowDestroying;
-        window.Created += OnWindowCreated;
-
         return window;
-    }
-
-    private void OnWindowCreated(object? sender, EventArgs e)
-    {
-        var window = sender as Window;
-        if (window?.Handler?.PlatformView is Microsoft.UI.Xaml.Window winWindow)
-        {
-            winWindow.AppWindow.Closing += (s, args) =>
-            {
-                args.Cancel = true;
-                winWindow.AppWindow.Hide();
-            };
-        }
-    }
-
-    private void OnWindowDestroying(object? sender, EventArgs e)
-    {
-        var window = sender as Window;
-        if (window?.Handler?.PlatformView is Microsoft.UI.Xaml.Window winWindow)
-        {
-            winWindow.AppWindow.Closing -= null;
-        }
     }
 }
